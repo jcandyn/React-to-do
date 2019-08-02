a142bc2b5ebf94d0ff4d4723295bd9a4dd5f8490
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-08-01 22:40:39 -0400

    changed component from functional to class

--- to-dos/src/components/List.js
+++ to-dos/src/components/List.js
@@ -4,11 +4,34 @@ import todosData from "./todoData.js"
 // import { tsPropertySignature } from "@babel/types";
 
 
-function List() {
-    const todoComponents = todosData.map(todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />)
-    return <div className="to-doList">
-            {todoComponents}
-            </div>
+// function List() {
+
+//     const todoComponents = todosData.map(
+//         todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />)
+//     return <div className="to-doList">
+//             {todoComponents}
+//             </div>
+// }
+
+class List extends React.Component {
+    // super()
+    // constructor() {
+    //     this.state = {
+    //         completed: true
+    //     }
+    // }
+    render() {
+        const todoComponents = todosData.map(
+            todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />)
+        return (
+             <div className="to-doList">
+                    {todoComponents}
+                    </div>
+        )
+    }
+
 }
+    
+
 
 export default List
